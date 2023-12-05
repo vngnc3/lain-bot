874cf47218b67f834c689e8195fdff3cfe7ed787
izzy <mista.rol@gmail.com>
2023-12-06 01:28:28 +0700

    basic bot setup

---
+++ index.js
@@ -0,0 +1,18 @@
+// Require the necessary discord.js classes
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+import 'dotenv/config'
+
+const token = process.env.DISCORD_TOKEN;
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once).
+// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
+// It makes some properties non-nullable.
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(token);
